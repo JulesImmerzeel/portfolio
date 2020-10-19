1823f930040acd2c10b11ec9dcdc08f3b8b67296
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-10-19 20:27:10 +0200

    Add project tags.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+function focusEvent(className) {
+    const elements = document.getElementsByClassName(className);
+    for(let element of elements){
+        element.addEventListener("click", () => {element.style.backgroundColor = 'pink'});
+        console.log('success')
+    }
+}
+
+
 window.onload = function skillAnimation() {
 
-    const skills = document.getElementsByClassName('progress-done')
+    const skills = document.getElementsByClassName('progress-done');
     for(let skill of skills) {
         skill.style.width = skill.getAttribute('data-done') + '%';
         skill.style.opacity = 1;
     }
+
+    focusEvent('progress-done')
+    focusEvent('event')
 }
+
+
