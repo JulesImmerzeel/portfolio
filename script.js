98d20a1326c4b04059692b722873c6c1249226b8
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-10-26 16:42:55 +0100

    Improve show() functionality.

--- script.js
+++ script.js
@@ -24,9 +24,10 @@ function hide() {
     document.getElementById("popup").style.display = "none";
 }
 
-function show(text) {
-    console.log("show")
-    document.querySelector("#popup > article").innerHTML = text
+function show(header,text, experience) {
+    document.querySelector("#popup > article > h3").innerHTML = header;
+    document.querySelector("#popup > article > p").innerHTML = text;
+    document.querySelector("#popup > article > ul").innerHTML = experience.map(x => '<li>' + x + '</li>').join('');
     document.getElementById("popup").style.display = "flex";
 
 }
