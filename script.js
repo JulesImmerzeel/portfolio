eaede70c4815ab037406e35241897495ed4d7436
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-11-07 16:16:25 +0100

    Add fade in / out animation to popup.

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ window.onload = function skillAnimation() {
 
 
 function hide() {
-    document.getElementById("popup").style.display = "none";
+    document.getElementById("popup").style.opacity = "0";
 }
 
 function show(header,text, experience) {
     document.querySelector("#popup > article > h3").innerHTML = header;
     document.querySelector("#popup > article > p").innerHTML = text;
     document.querySelector("#popup > article > ul").innerHTML = experience.map(x => '<li>' + x + '</li>').join('');
-    document.getElementById("popup").style.display = "flex";
+    document.getElementById("popup").style.opacity = "100";
 
 }
