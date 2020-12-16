788123861626fede3a6a9ae0a3ab698b1ef0815c
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-12-16 20:27:43 +0100

    Fix invisible popup blocking timeline and skills buttons.

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 function hide() {
-    document.getElementById("popup").style.opacity = "0";
+    document.getElementById("popup").style.display = "none";
 }
 
 function show(header,text, experience) {
     document.querySelector("#popup > article > h3").innerHTML = header;
     document.querySelector("#popup > article > p").innerHTML = text;
     document.querySelector("#popup > article > ul").innerHTML = experience.map(x => '<li>' + x + '</li>').join('');
-    document.getElementById("popup").style.opacity = "100";
+    document.getElementById("popup").style.display = "block";
 }
