8f164d33ddecd6152f862f91c6fcb2a1ed0ab4ef
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-12-19 00:02:09 +0100

    Replace skill animation function with css animations.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 import typewriter from './typewriter.js';
 
 window.onload = async () => {
-    skillAnimation();
     await typewriter("Jules Immerzeel", "header-name");
     typewriter("A Computer Science student", "header-subtext");
-}
-
-function skillAnimation () {
-    const skills = document.getElementsByClassName('progress-done');
-    for(let skill of skills) {
-        skill.style.width = skill.getAttribute('data-done') + '%';
-        skill.style.opacity = 1;
-    }
 }
