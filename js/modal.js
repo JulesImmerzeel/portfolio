1c8fde4fb112cfb14a27c93dc894a9cf0fd4919c
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-04-13 22:48:42 +0200

    Content voor de projecten geschreven.

--- js/modal.js
+++ js/modal.js
@@ -3,8 +3,9 @@ function hide () {
 }
 
 function show (header,text, experience) {
-    document.querySelector("#modal > article > h3").innerHTML = header;
-    document.querySelector("#modal > article > p").innerHTML = text;
-    document.querySelector("#modal > article > ul").innerHTML = experience.map(x => '<li>' + x + '</li>').join('');
+    document.querySelector("#modal > h3").innerHTML = header;
+    document.querySelector("#modal > p").innerHTML = text;
+    document.querySelector("#modal > ul").innerHTML = experience.map(x => '<li>' + x + '</li>').join('');
     document.getElementById("modal").style.display = "block";
 }
+
--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ import typewriter from './typewriter.js';
 window.onload = async () => {
     await typewriter("Jules Immerzeel", "header-name");
     typewriter("A Computer Science student", "header-subtext");
+
+    let container = document.querySelector(".container").getBoundingClientRect();
+    document.getElementById("modal").style.left = `${container.left}px`;
 }
