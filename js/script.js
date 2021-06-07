8f2781bc2ef6162750740f34b0624d1beed88505
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-06-07 16:18:07 +0200

    add content.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import typewriter from './typewriter.js';
 
 window.onload = async () => {
     await typewriter("Jules Immerzeel", "header-name");
-    typewriter("A Computer Science student", "header-subtext");
+    typewriter("Student informatica", "header-subtext");
 
     let container = document.querySelector(".container").getBoundingClientRect();
     document.getElementById("modal").style.left = `${container.left}px`;
